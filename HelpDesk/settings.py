8ef1072a4ebf96d0e87dbca5ce2a38ed360768a4
mehedishovon01 <mehedishovon01@gmail.com>
2023-11-13 03:45:18 +0600

    Template Mastering

--- HelpDesk/settings.py
+++ HelpDesk/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Installed Apps
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'HelpDesk.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/ 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,8 +131,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    BASE_DIR/'static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media Files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # 'media' is my media folder
+MEDIA_URL = '/media/'
+
+# Installed Library
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+# Login redirection
+LOGIN_REDIRECT_URL = 'profile'
+LOGIN_URL = 'login'
---
+++ user/urls.py
@@ -0,0 +1,18 @@
+"""
+URL configuration for main project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+"""
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path('', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(template_name='auth/logout.html'), name='logout'),
+    path('dashboard/', views.profile, name='dashboard'),
+    # path('add/', views.add, name='add'),
+    # path('registration/', views.registration, name='registration'),
+]
